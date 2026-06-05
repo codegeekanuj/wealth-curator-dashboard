5298c20188092d53f39eeafbdf0725ee7b40e3c8
Anuj Tripathi <114897668+codegeekanuj@users.noreply.github.com>
2026-06-05 11:05:21 +0530

    prod updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: /^lodash\/(.*)/, replacement: 'lodash-es/$1' },
+      { find: 'lodash', replacement: 'lodash-es' }
+    ]
+  },
 })
+
