337681a433eefb203da0b17abea6c3b4d8039306
Anuj Tripathi <114897668+codegeekanuj@users.noreply.github.com>
2026-06-05 11:15:42 +0530

    fix: resolve es-toolkit/compat import default crashes in production bundle

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
+      { find: /^es-toolkit\/compat\/(.*)/, replacement: path.resolve(__dirname, 'src/compat/$1.js') },
       { find: /^lodash\/(.*)/, replacement: 'lodash-es/$1' },
       { find: 'lodash', replacement: 'lodash-es' }
     ]
   },
 })
+
 
